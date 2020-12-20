829e92c929203387f3d35169b193dae48e80d799
Zukson <kapiolimpia@wp.pl>
2020-12-20 18:50:59 +0100

    Added Contracts for Api

---
+++ MyShop.API.Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyShop.API.Contracts.V1
+{
+  public static  class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+
+
+        public const string Base = Root + "/" + Version;
+        public static class Products
+        {
+            public const string ProductRoot = "products";
+            public const string GetAllProducts = Base + ProductRoot;
+            public const string GetProductById = Base + ProductRoot + "{productId}";
+
+        }
+    }
+
+   
+
+}
